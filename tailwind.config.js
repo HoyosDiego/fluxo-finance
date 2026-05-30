020d49c45bd87ecde45decd33f3b3a222dd68918
HoyosDiego <dfhoyos29@gmail.com>
2026-05-30 09:39:34 -0500

    feat(app): enhance setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./app/**/*.{js,jsx,ts,tsx}",
-        "./shared/**/*.{js,jsx,ts,tsx}",
-        "./features/**/*.{js,jsx,ts,tsx}",
+        "./src/app/**/*.{js,jsx,ts,tsx}",
+        "./src/components/**/*.{js,jsx,ts,tsx}",
+        "./src/features/**/*.{js,jsx,ts,tsx}",
+        "./src/shared/**/*.{js,jsx,ts,tsx}",
     ],
     presets: [require("nativewind/preset")],
     theme: {
